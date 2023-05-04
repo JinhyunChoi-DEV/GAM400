cd5911473a28411cbeed0fd964caf907372313e3
Jinhyun Choi <jinhyun.choi@digipen.edu>
2023-05-04 12:25:17 -0700

    temporarily remove error code

--- GAM400_Action_RPG/Assets/Scripts/Character/Move/CharacterMoveStateMachine.cs
+++ GAM400_Action_RPG/Assets/Scripts/Character/Move/CharacterMoveStateMachine.cs
@@ -2,43 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: Maybe Monobehavior 
-public class CharacterData
-{
+//// TODO: Maybe Monobehavior 
+//public class CharacterData
+//{
 
-}
+//}
 
-public class WalkState : IState
-{
-    private StateMachine baseMachine;
+//public class WalkState : IState
+//{
+//    private StateMachine baseMachine;
 
-    public WalkState(CharacterData characterData, StateMachine baseMachine)
-    {
-        this.baseMachine = baseMachine;
-    }
-
-
-}
-
-public class JumpState : IState
-{
-    private StateMachine baseMachine;
-
-    public JumpState(CharacterData characterData, StateMachine baseMachine)
-    {
-        this.baseMachine = baseMachine;
-    }
-}
+//    public WalkState(CharacterData characterData, StateMachine baseMachine)
+//    {
+//        this.baseMachine = baseMachine;
+//    }
 
 
-public class CharacterMoveStateMachine : StateMachine
-{
-    private IState baseState;
+//}
 
-    public CharacterMoveStateMachine(IState state)
-    {
-        baseState = state;
-    }
+//public class JumpState : IState
+//{
+//    private StateMachine baseMachine;
+
+//    public JumpState(CharacterData characterData, StateMachine baseMachine)
+//    {
+//        this.baseMachine = baseMachine;
+//    }
+//}
+
+
+//public class CharacterMoveStateMachine : StateMachine
+//{
+//    private IState baseState;
+
+//    public CharacterMoveStateMachine(IState state)
+//    {
+//        baseState = state;
+//    }
 
     
-}
+//}
