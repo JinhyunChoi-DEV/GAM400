354a4580a259e6e5d84f5abe79aef805f182f546
ethankamei <ethan.kamei@digipen.edu>
2023-06-01 11:59:15 -0700

    6/1

--- GAM400_Action_RPG/Assets/Scripts/VFX/Testing/VFXTestPlayer.cs
+++ GAM400_Action_RPG/Assets/Scripts/VFX/Testing/VFXTestPlayer.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXTestPlayer : MonoBehaviour
 {
+    [SerializeField] private KeyCode key;
+    [SerializeField] private VisualEffect[] vfxGraph;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class VFXTestPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Mathf.Repeat(1, 1);
+        if (Input.GetKeyDown(key))
+        {
+            for (int i = 0; i < vfxGraph.Length; ++i)
+            {
+                if (vfxGraph[i] != null)
+                    vfxGraph[i].Play();
+            }
+        }
     }
+
 }
